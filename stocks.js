6edb2c2eb75d80e84c21aee69d6e349f9f3efbb5
Ryan Yang <Ryanyang99@hotmail.com>
2018-03-28 14:30:24 -0700

    Update Email

--- stocks.js
+++ stocks.js
@@ -29,5 +29,28 @@ axios.get('https://www.bloomberg.com/quote/SGH:US')
 
 
 
-// Push Notifications
-var push = require( 'pushsafer-notifications' );
+// Email Notifications
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'ryang20718@likelion.org',
+    pass: 'Pass'
+  }
+});
+
+var mailOptions = {
+  from: 'ryang20718@likelion.org',
+  to: 'siru@2ether.net',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
