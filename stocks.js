d464d709e0d85bec1bd608bd8283547c604c24b1
Ryan Yang <Ryanyang99@hotmail.com>
2018-03-27 20:49:44 -0700

    Update Scraping

--- stocks.js
+++ stocks.js
@@ -8,7 +8,7 @@ axios.get('https://www.bloomberg.com/quote/SGH:US')
             const html = response.data;
             const $ = cheerio.load(html);
             let devtoList = [];
-           
+
             $('#root .contentWell__b0c648c9 .quotePageSnapshot .pseudoMainContent .snapshot__0569338b .snapshotOverview__d5769afc .snapshotSummary__16511a01 .price__c3a38e1d .overviewRow__0956421f').each(function (i, elem) {
                 devtoList[i] = {
                     Price: $(this).find('span').text().trim(),
@@ -19,9 +19,17 @@ axios.get('https://www.bloomberg.com/quote/SGH:US')
                         })
                 }
             });
-            
-            
-            
+
+            devtoList[2] = {
+                Price: $('#root .contentWell__b0c648c9 .quotePageSnapshot .pseudoMainContent .snapshot__0569338b .snapshotOverview__d5769afc .snapshotSummary__16511a01 .price__c3a38e1d .overviewRow__0956421f').find('span').text().trim(),
+                Flux: $(this).find('span').text().split('#')
+                    .map(tag => tag.trim())
+                    .filter(function (n) {
+                        return n != ""
+                    })
+            }
+
+
             const devtoListTrimmed = devtoList.filter(n => n != undefined)
             fs.writeFile('output.json',
                 JSON.stringify(devtoListTrimmed, null, 4),
