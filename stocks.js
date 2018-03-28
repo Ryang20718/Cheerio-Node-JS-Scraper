fd31d511fd3922cad82a7ea199fc73ccc6113b77
Ryan Yang <Ryanyang99@hotmail.com>
2018-03-28 12:29:07 -0700

    Update

--- stocks.js
+++ stocks.js
@@ -28,32 +28,6 @@ axios.get('https://www.bloomberg.com/quote/SGH:US')
     }, (error) => console.log(err));
 
 
-function notifyMe() {
-  if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-  }
-  else if (Notification.permission === "granted") {
-        var options = {
-                body: "This is the body of the notification",
-                icon: "icon.jpg",
-                dir : "ltr"
-             };
-          var notification = new Notification("Hi there",options);
-  }
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-      if (!('permission' in Notification)) {
-        Notification.permission = permission;
-      }
-    
-      if (permission === "granted") {
-        var options = {
-              body: "This is the body of the notification",
-              icon: "icon.jpg",
-              dir : "ltr"
-          };
-        var notification = new Notification("Hi there",options);
-      }
-    });
-  }
-}
+
+// Push Notifications
+var push = require( 'pushsafer-notifications' );
