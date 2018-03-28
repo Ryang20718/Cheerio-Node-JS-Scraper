8cb74214a436b9263e8421b9fd957226de8f67e8
Ryan Yang <Ryanyang99@hotmail.com>
2018-03-28 12:28:36 -0700

    Update

--- stocks.js
+++ stocks.js
@@ -9,21 +9,14 @@ axios.get('https://www.bloomberg.com/quote/SGH:US')
             const $ = cheerio.load(html);
             let devtoList = [];
 
-            $('#root .contentWell__b0c648c9 .quotePageSnapshot .pseudoMainContent .snapshot__0569338b .snapshotOverview__d5769afc .snapshotSummary__16511a01 .price__c3a38e1d .overviewRow__0956421f').each(function (i, elem) {
-                devtoList[i] = {
-                    Price: $(this).find('span').text().trim(),
-                    Flux: $(this).find('span').text().split('#')
-                        .map(tag => tag.trim())
-                        .filter(function (n) {
-                            return n != ""
-                        })
-                }
-            });
-
-            devtoList[2] = {
-                Price: $('#root .contentWell__b0c648c9 .quotePageSnapshot .pseudoMainContent .snapshot__0569338b .snapshotOverview__d5769afc .snapshotSummary__16511a01 .price__c3a38e1d .overviewRow__0956421f').find('span').first().text().trim(),
+            devtoList[0] = {
+                Stock:$('#root .contentWell__b0c648c9 .quotePageSnapshot .pseudoMainContent .snapshot__0569338b .snapshotOverview__d5769afc .snapshotSummary__16511a01 .company__c1979f17').find('h1').text().trim(),
                 
-                amtChanged: $('#root .contentWell__b0c648c9 .quotePageSnapshot .pseudoMainContent .snapshot__0569338b .snapshotOverview__d5769afc .snapshotSummary__16511a01 .price__c3a38e1d .overviewRow__0956421f').find('span').eq(2).text().trim()
+                Price: $('#root .contentWell__b0c648c9 .quotePageSnapshot .pseudoMainContent .snapshot__0569338b .snapshotOverview__d5769afc .snapshotSummary__16511a01 .price__c3a38e1d .overviewRow__0956421f').find('span').first().text().trim(),
+
+                amtChanged: $('#root .contentWell__b0c648c9 .quotePageSnapshot .pseudoMainContent .snapshot__0569338b .snapshotOverview__d5769afc .snapshotSummary__16511a01 .price__c3a38e1d .overviewRow__0956421f').find('span').eq(2).text().trim(),
+
+                pctChanged: $('#root .contentWell__b0c648c9 .quotePageSnapshot .pseudoMainContent .snapshot__0569338b .snapshotOverview__d5769afc .snapshotSummary__16511a01 .price__c3a38e1d .overviewRow__0956421f').find('span').eq(3).text().trim()
             }
 
 
@@ -33,3 +26,34 @@ axios.get('https://www.bloomberg.com/quote/SGH:US')
                 (err) => console.log('File successfully written!'))
         }
     }, (error) => console.log(err));
+
+
+function notifyMe() {
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+  else if (Notification.permission === "granted") {
+        var options = {
+                body: "This is the body of the notification",
+                icon: "icon.jpg",
+                dir : "ltr"
+             };
+          var notification = new Notification("Hi there",options);
+  }
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      if (!('permission' in Notification)) {
+        Notification.permission = permission;
+      }
+    
+      if (permission === "granted") {
+        var options = {
+              body: "This is the body of the notification",
+              icon: "icon.jpg",
+              dir : "ltr"
+          };
+        var notification = new Notification("Hi there",options);
+      }
+    });
+  }
+}
