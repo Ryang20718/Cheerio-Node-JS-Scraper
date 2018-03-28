85325e03956ef6dff2fe7a8a6cf8b92d9da5af64
Ryan Yang <Ryanyang99@hotmail.com>
2018-03-27 17:37:16 -0700

    Update

--- stocks.js
+++ stocks.js
@@ -2,17 +2,17 @@ let axios = require('axios');
 let cheerio = require('cheerio');
 let fs = require('fs');
 
-axios.get('https://finance.yahoo.com/quote/SGH?p=SGH')
+axios.get('https://www.bloomberg.com/quote/SGH:US')
     .then((response) => {
         if (response.status === 200) {
             const html = response.data;
             const $ = cheerio.load(html);
             let devtoList = [];
            
-            $('#app .render-target-active #YDC-Lead #quote-header-info .My(6px) ').each(function (i, elem) {
+            $('#root .contentWell__b0c648c9 .quotePageSnapshot .pseudoMainContent .snapshot__0569338b .snapshotOverview__d5769afc .snapshotSummary__16511a01 .price__c3a38e1d .overviewRow__0956421f').each(function (i, elem) {
                 devtoList[i] = {
-                    Dining: $(this).find('span').text().trim(),
-                    tags: $(this).find('span').text().split('#')
+                    Price: $(this).find('span').text().trim(),
+                    Flux: $(this).find('span').text().split('#')
                         .map(tag => tag.trim())
                         .filter(function (n) {
                             return n != ""
